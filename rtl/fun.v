0b6c0f6bd8e598f5bdcdc49b24c05d5c696df584
homer.xing <homer.xing@511c7516-f88b-48a8-9e95-2656910a1732>
2012-02-23 04:35:30 +0000

    LGPL header

--- rtl/fun.v
+++ rtl/fun.v
@@ -15,7 +15,7 @@
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see http://www.gnu.org/licenses/lgpl.txt
+    along with Tate Bilinear Pairing Core.  If not, see http://www.gnu.org/licenses/lgpl.txt
 */
 
 // fun.v: Have you got fun reading the code ?
